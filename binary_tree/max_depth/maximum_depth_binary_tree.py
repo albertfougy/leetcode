f4b06901bd4c16fc172729de272489cc79ad67e2
Albert Fougy <albert@fougy.com>
2020-02-17 06:53:20 -0500

    addition of binary tree node definition

--- binary_tree/max_depth/maximum_depth_binary_tree.py
+++ binary_tree/max_depth/maximum_depth_binary_tree.py
@@ -18,6 +18,13 @@
 
 # return its depth = 3.
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
         if not root:
