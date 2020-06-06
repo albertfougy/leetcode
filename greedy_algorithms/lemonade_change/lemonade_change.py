d6ace61abb6406570805ee13c2b2f3c189f6e3c4
Albert Fougy <albert@fougy.com>
2020-06-06 11:26:55 -0400

    readable greedy solution updated

--- greedy_algorithms/lemonade_change/lemonade_change.py
+++ greedy_algorithms/lemonade_change/lemonade_change.py
@@ -1,19 +1,15 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
         five = ten = 0
-        for bill in bills:
-            if bill == 5:
+        for i in bills:
+            if i == 5: 
                 five += 1
-            elif bill == 10:
-                if not five: return False
-                five -= 1
-                ten += 1
-            else:
-                if ten and five:
-                    ten -= 1
-                    five -= 1
-                elif five >= 3:
-                    five -= 3
-                else:
-                    return False
+            elif i == 10: 
+                five, ten = five - 1, ten + 1
+            elif ten > 0: 
+                five, ten = five - 1, ten - 1
+            else: 
+                five -= 3
+            if five < 0: 
+                return False
         return True
